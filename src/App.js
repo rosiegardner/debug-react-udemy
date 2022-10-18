4409a2543da7d06ccac6a6f187364018f41d1305
Rosie Gardner <rosiegardner78@gmail.com>
2022-10-18 12:11:49 -0700

    complete: section7 - 'debugging react apps'

--- src/App.js
+++ src/App.js
@@ -10,15 +10,15 @@ const App = () => {
     { text: 'Finish the course!', id: 'g2' }
   ]);
 
-  const addGoalHandler = enteredText => {
+  const addGoalHandler = (enteredText) => {
     setCourseGoals(prevGoals => {
       const updatedGoals = [...prevGoals];
-      updatedGoals.unshift({ text: enteredText, id: 'goal1' });
+      updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
       return updatedGoals;
     });
   };
 
-  const deleteItemHandler = goalId => {
+  const deleteItemHandler = (goalId) => {
     setCourseGoals(prevGoals => {
       const updatedGoals = prevGoals.filter(goal => goal.id !== goalId);
       return updatedGoals;
